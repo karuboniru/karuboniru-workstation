83e30feca76cf3b731a4fa16c987199f579366a0
Karuboniru <yanqiyu01@gmail.com>
2026-08-27 10:06:36 +0800

    Use C++20 formatting for CVMFS units

--- finalize.d/0003-install-generator.sh
+++ finalize.d/0003-install-generator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 clang++ $RPMOSTREE_WORKDIR/extra-source/systemd-cvmfs-generator.cpp \
-        -O2 -std=c++17 \
+        -O2 -std=c++20 \
         -o ./usr/lib/systemd/system-generators/systemd-cvmfs-generator
